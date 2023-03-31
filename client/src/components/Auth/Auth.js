20d376afab2ac616868c9d7839202409a1bb136a
John Allan Hinds <jabistro@gmail.com>
2023-03-31 11:27:32 -0400

    user model added, userRoutes added, user controllers and actions added

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -10,26 +10,46 @@ import {
 import { GoogleLogin } from "react-google-login";
 import { gapi } from "gapi-script";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Input from "./Input";
-
-import useStyles from "./styles";
-import Icon from "./Icon";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+
+import Input from "./Input";
+import useStyles from "./styles";
+import Icon from "./Icon";
+import { signin, signup } from "../../actions/auth";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Auth = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
   const [isSignUp, setIsSignUp] = useState(false);
+  const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleShowPassword = () =>
     setShowPassword((prevShowPassword) => !prevShowPassword);
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleChange = () => {};
+    if (isSignUp) {
+      dispatch(signup(formData, history));
+    } else {
+      dispatch(signin(formData, history));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const switchMode = () => {
     setIsSignUp((prevIsSignUp) => !prevIsSignUp);
